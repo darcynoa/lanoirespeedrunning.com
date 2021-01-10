3f9b07eafce9146f5ea1c4a6808ab697a17d141f
Hydrox48 <theprimetimer100@gmail.com>
2021-01-09 20:19:03 -0500

    Really unnecessary

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,24 +1,31 @@
-function openPage( pageName, elmnt, color) {
-    
-    var i, tabcontent, tablinks;
-    
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "";
-        tablinks[i].style.color = "";
-    }
+//function openPage( pageName, elmnt, color) {
+//
+//    var i, tabcontent, tablinks;
+//
+//    tabcontent = document.getElementsByClassName("tabcontent");
+//    for (i = 0; i < tabcontent.length; i++) {
+//        tabcontent[i].style.display = "none";
+//    }
+//
+//    tablinks = document.getElementsByClassName("tablink");
+//    for (i = 0; i < tablinks.length; i++) {
+//        tablinks[i].style.backgroundColor = "";
+//        tablinks[i].style.color = "";
+//    }
+//
+//
+//    document.getElementById(pageName).style.display = "block";
+//
+//    elmnt.style.backgroundColor = color;
+//    elmnt.style.color = '#ffbf00';
+//
+//}
+//
+//document.getElementById("defaultOpen").click();
 
-    
-    document.getElementById(pageName).style.display = "block";
-    
-    elmnt.style.backgroundColor = color;
-    elmnt.style.color = '#ffbf00';
-    
-}
-
-document.getElementById("defaultOpen").click();
+document.querySelector('.category-img').addEventListener("click", function()
+{
+    document.querySelector('.category-img').style.transform = "rotateY(90deg)";
+    console.log("Rotated")
+    document.querySelector('#test1').style.transform = "rotateY(180deg)";
+});
