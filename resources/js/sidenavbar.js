adc78fb7c36faf5ca9bfd2bbb395493bcefef97a
darcynoa <darcynoa@msu.edu>
2021-03-05 21:28:23 -0500

    Improving readability of sidenav code

--- resources/js/sidenavbar.js
+++ resources/js/sidenavbar.js
@@ -1,26 +1,27 @@
-var acc = document.getElementsByClassName("accordion");
+var tabs = document.getElementsByClassName("accordion");
 var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+/**
+ * For each tab, add the .active class to the tab. Open up the panel to its maximum height + 50px
+ */
+for (i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
+    if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "50px";
-    } 
+    }
   });
 }
 
-
-
 function openNav() {
-    document.getElementById("mySidenav").style.width = "320px";
-    document.getElementById("btnSidenav").style.zIndex = '0';
+  document.getElementById("mySidenav").style.width = "320px";
+  document.getElementById("btnSidenav").style.zIndex = "0";
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0px";
-    document.getElementById("btnSidenav").style.display = "block";
+  document.getElementById("mySidenav").style.width = "0px";
+  document.getElementById("btnSidenav").style.display = "block";
 }
