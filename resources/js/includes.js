82355415177fd63f07b61eafbd31abd31a55d9e4
darcynoa <darcynoa@msu.edu>
2021-03-05 20:34:47 -0500

    More accessbility to the includes

--- resources/js/includes.js
+++ resources/js/includes.js
@@ -1,5 +1,11 @@
 $("#sidenav").load("includes/sidenav.html");
+$("#sidenavParent").load("../includes/sidenav.html");
+$("#sidenavParentParent").load("../../includes/sidenav.html");
             
 $("#footer").load("includes/footer.html"); 
+$("#footerParent").load("../includes/footer.html");
+$("#footerParentParent").load("../../includes/footer.html");
 
-$("#scroll-to-top").load("includes/scroll-to-top.html"); 
+$("#scroll-to-top").load("includes/scroll-to-top.html");
+$("#scroll-to-topParent").load("../includes/scroll-to-top.html");
+$("#scroll-to-topParentParent").load("../../includes/scroll-to-top.html");
